413e15052fe8b9ed729a5b80c63fe3a3961762e8
临黑 <linhei714@qq.com>
2023-05-26 21:40:07 +0800

    为@Value配置默认值   修改updateConfigProperty方法

--- src/main/java/com/linhei/autoupdatednsrecord/server/impl/MailServerImpl.java
+++ src/main/java/com/linhei/autoupdatednsrecord/server/impl/MailServerImpl.java
@@ -16,6 +16,7 @@ import org.yaml.snakeyaml.Yaml;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
@@ -30,14 +31,16 @@ public class MailServerImpl implements MailServer {
     @Autowired
     private MailProperties mailProperties;
 
-    @Value("${mail_server.last_size}")
-    private String lastSize = "-1";
+    @Value("${mail_server.last_size:-1}")
+    private String lastSize;
     /**
      * 收件箱持续连接
      * File 打开文件
+     * Map用于处理子字段
      */
     Folder inbox = null;
     File file = new File("./external.yml");
+    private Map<String, Object> valueMap;
 
     @Override
     @Scheduled(fixedDelay = 5000)
@@ -50,11 +53,6 @@ public class MailServerImpl implements MailServer {
             properties.setProperty("mail.pop3s.username", mailProperties.getUsername());
             properties.setProperty("mail.pop3s.password", mailProperties.getPassword());
             mailProperties.getProperties().forEach(properties::setProperty);
-/*            this.store = Session.getDefaultInstance(properties).getStore();
-            this.host = mailProperties.getHost();
-            this.user = mailProperties.getUsername();
-            this.password = mailProperties.getPassword();
-            store.connect(this.host, this.user, this.password);*/
             Store store = Session.getDefaultInstance(properties).getStore();
             store.connect(mailProperties.getHost(), mailProperties.getUsername(), mailProperties.getPassword());
             inbox = store.getFolder("INBOX");
@@ -90,7 +88,12 @@ public class MailServerImpl implements MailServer {
         Map<String, Object> yamlMap = yaml.load(yamlContent);
 
         // 更新指定键的值
-        yamlMap.put("mail_server.last_size", "1");
+        String[] split = key.split("\\.");
+        String mapKey = split[0];
+        if (this.valueMap == null)
+            valueMap = (Map<String, Object>) yamlMap.getOrDefault(mapKey, new HashMap<>(1));
+        valueMap.put(split[1], value);
+        yamlMap.put(split[0], valueMap);
 
         // 转换为更新后的 YAML 字符串
         String updatedYamlContent = yaml.dump(yamlMap);
